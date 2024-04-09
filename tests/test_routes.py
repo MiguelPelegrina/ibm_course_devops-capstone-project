c86e49d3d3eca86ddae96161b6a51382aa1e289f
MiguelPelegrina <migupele@gmail.com>
2024-04-09 06:16:28 -0400

    added update account

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -133,12 +133,23 @@ class TestAccountService(TestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         data = response.get_json()
         self.assertEqual(data["name"], account.name)
-        # make a call to self.client.post() to create the account
-        # assert that the resp.status_code is status.HTTP_200_OK
-        # get the data from resp.get_json()
-        # assert that data["name"] equals the account.name
 
     def test_get_account_not_found(self):
         """It should not Read an Account that is not found"""
         response = self.client.get(f"{BASE_URL}/0")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_update_account(self):
+        """It should Update an existing Account"""
+        # create an Account to update
+        test_account = AccountFactory()
+        response = self.client.post(BASE_URL, json=test_account.serialize())
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # update the account
+        new_account = response.get_json()
+        new_account["name"] = "New name"
+        response = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        updated_account = response.get_json()
+        self.assertEqual(updated_account["name"], "New name")
