46cfd8dcf7003fe5d75e83c480bed8aaf782c1e4
MiguelPelegrina <95084406+MiguelPelegrina@users.noreply.github.com>
2024-04-09 13:38:26 +0200

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -86,7 +86,7 @@ def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")    
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
 
